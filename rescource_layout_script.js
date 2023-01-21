232c7c420f285c1bf61ca0aa3da2100d668c5dd4
mahojo99 <mejohan@hiof.no>
2023-01-21 19:21:56 +0100

    more css and added js

---
+++ rescource_layout_script.js
@@ -0,0 +1,127 @@
+const resources = [
+    {
+        category: "HTML",
+        text: "HTML står for HyperText Markup Language, og er et strukturspråk som brukes for å lage strukturer til nettside- og applikasjonsgrensesnitt.",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/html/"
+            },
+            {
+                title: "HTML Living standard",
+                url: "https://html.spec.whatwg.org/multipage/"
+            },
+            {
+                title: "HTML.com Tutorials",
+                url: "https://html.com/"
+            },
+        ]
+    },
+    {
+        category: "CSS",
+        text: "CSS står for Cascading StyleSheets, og brukes for å sette stilregler på HTML-elementer.",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/css/"
+            },
+            {
+                title: "W3C HTML & CSS Standards",
+                url: "https://www.w3.org/standards/webdesign/htmlcss.html"
+            },
+            {
+                title: "W3C CSS Validator",
+                url: "https://jigsaw.w3.org/css-validator/"
+            },
+            {
+                title: "CSS Tricks",
+                url: "https://css-tricks.com/"
+            },
+        ]
+    },
+    {
+        category: "JavaScript",
+        text: "JavaScript er et scriptspråk basert på EcmaScript. JavaScript kjører direkte i nettleseren, og brukes ofte til å manipulere HTML og CSS i webgrensnesnitt.",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/js/"
+            },
+            {
+                title: "MDN Web Docs",
+                url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            },
+            {
+                title: "How to read JavaScript Documentation",
+                url: "https://www.youtube.com/watch?v=O3iR-CIufKM"
+            },
+        ]
+    },
+    {
+        category: "React",
+        text: "React er et rammeverk bygget i JavaScript. React bruker komponenter og states for å lage en levende frontend.",
+        sources: [
+            {
+                title: "React documentation",
+                url: "https://reactjs.org/docs/getting-started.html"
+            },
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/REACT/DEFAULT.ASP"
+            },
+            {
+                title: "How to read JavaScript Documentation",
+                url: "https://www.youtube.com/watch?v=O3iR-CIufKM"
+            },
+        ]
+    },
+    {
+        category: "Sanity and headless CMS",
+        text: "Sanity er et headless CMS som står for innholdsadministrasjon. Innhold hentes inn i applikasjoner via GROQ-spørringer.",
+        sources: [
+            {
+                title: "Sanity documentation",
+                url: "https://www.sanity.io/docs"
+            },
+            {
+                title: "OnCrawl: a beginners guide to headless CMS",
+                url: "https://www.oncrawl.com/technical-seo/beginners-guide-headless-cms/"
+            },
+            {
+                title: "Section.io: Getting started with Sanity CMS",
+                url: "https://www.section.io/engineering-education/getting-started-with-sanity-cms/"
+            },
+        ]
+    },
+]
+
+/* 
+function listResources() {
+    let resourceListHTML = ""
+    resources.map(prod => {
+        resourceListHTML += `
+        <nav>
+            <a class="active" href="kategori1.html">Kategori 1</a>
+            <a href="kategori2.html">Kategori 2</a>
+            <a href="kategori2.html">Kategori 3</a>
+            <a href="kategori2.html">Kategori 4</a>
+            <a href="kategori2.html">Kategori 5</a>
+        </nav>
+        <main>
+            <h2>Kategori 1</h2>
+            <article>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Praesent placerat, nisl non venenatis vestibulum, nulla diam tempor est, ut vehicula elit dui eu elit. 
+                    Pellentesque quis mauris ipsum. Morbi sed lacus sit amet eros porta hendrerit. 
+                    Nunc pretium ligula in imperdiet pretium. Nunc nec lorem lobortis, porta massa quis, egestas metus. 
+                    Vivamus non est at lectus facilisis rutrum nec ac odio. Sed pulvinar placerat arcu vitae dignissim.
+                </p>
+            </article>
+        `
+    })
+    document.getElementById("product-list").innerHTML = resourceListHTML
+}
+//Kjør opplisting av produkter så de synes når vi laster inn siden
+listProducts()
+
+*/
